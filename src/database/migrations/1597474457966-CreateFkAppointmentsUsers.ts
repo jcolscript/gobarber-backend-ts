d4b258e34c021fb5967234621eb7ea6866f1c684
José Carlos <jcolscript@gmail.com>
2020-08-15 04:09:58 -0300

    criando model e migrations de usuario

---
+++ src/database/migrations/1597474457966-CreateFkAppointmentsUsers.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateFkAppointmentsUsers1597474457966
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'appointments',
+      new TableForeignKey({
+        name: 'fk_appointments_users',
+        columnNames: ['provider_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'fk_appointments_users');
+  }
+}
